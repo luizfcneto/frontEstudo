25c01b6705e41f28c5aae4bddd90b1b28bd48a54
luizfcneto <luizfcneto123@gmail.com>
2020-05-26 15:27:41 -0300

    +Sprint 3 DONE - RemoveData, UI, DtController

--- JavaScript/handsOn1(m)/js/app-es5.js
+++ JavaScript/handsOn1(m)/js/app-es5.js
@@ -15,9 +15,12 @@
     Array.shift():
         - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
         
+    Array.splice():
+        - https://www.w3schools.com/jsref/jsref_splice.asp
+        - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+
+
 */
-
-
 
 console.log( "arquivo app-es5.js carregado" );
 
@@ -30,6 +33,7 @@ var UiController = ( function () {
         inputDescription: document.querySelector( ".add__description" ),
         inputValue: document.querySelector( ".add__value" ),
         btn: document.querySelector( ".add__btn" ),
+        form: document.querySelector( ".add__container" ),
 
         rendimentoList: document.querySelector( ".income__list" ),
         despesaList: document.querySelector( ".expenses__list" ),
@@ -151,6 +155,48 @@ var UiController = ( function () {
         return element + "%";
     }
 
+    var searchElement = function( id, type ){
+        if ( type === "rendimento" ){
+            var element;
+            var list = DOMElements.rendimentoList;
+
+            for( var i = 0; i < list.children.length; i++ ){
+                if( list.children[i].id === "income-" + id ){
+                    element = list.children[i]
+                    break;
+                }
+
+            }
+            return element;
+
+
+        }else if ( type === "despesa" ){
+            var element;
+            var list = DOMElements.despesaList;
+
+            for ( var i = 0; i < list.children.length; i++ ){
+                if( list.children[i].id === "expense-" + id ){
+                    element = list.children[i];
+                    break;
+                }
+            }
+
+            return element
+
+
+        } else {
+            console.log( "tipo não encontrado" );
+        }
+
+
+    }
+
+    var removeUIData = function( id, type ) {
+        var elementToRemove = searchElement( id, type )
+        elementToRemove.parentNode.removeChild( elementToRemove );
+
+    }
+
     // Métodos públicos
     return {
         getDOMElements: function (){
@@ -169,14 +215,12 @@ var UiController = ( function () {
 
         addUiData: function( data, total, type ){
             var elementoNovo;
-
-
             
             if( type === "rendimento" ){
                 elementoNovo = rendimentoDOMElement;
 
                 // Renomeando id:
-                elementoNovo = elementoNovo.replace( "%id%", total.rendimentos );
+                elementoNovo = elementoNovo.replace( "%id%", total.rendimentos - 1 );
 
                 // Renomeando descrição:
                 elementoNovo = elementoNovo.replace( "%description%", data.listaRendimento[ data.listaRendimento.length - 1 ].description );
@@ -192,7 +236,7 @@ var UiController = ( function () {
                 elementoNovo = despesaDOMElement;
                 
                 // Renomeando Id:
-                elementoNovo = elementoNovo.replace( "%id%", total.despesas );
+                elementoNovo = elementoNovo.replace( "%id%", total.despesas - 1 );
                 
                 // Renomeando descrição:
                 elementoNovo = elementoNovo.replace( "%description%", data.listaDespesa[ data.listaDespesa.length - 1 ].description );
@@ -218,6 +262,10 @@ var UiController = ( function () {
                 updateDisplayDespesasPercentages( data.listaDespesa );
             }
 
+        },
+
+        removeData: function( id, type ){
+            removeUIData( id, type );
         }
 
     } 
@@ -270,6 +318,104 @@ var DataController = ( function() {
 
     }
 
+    // CONSERTAR!!!
+    var removeDataFromList = function( id, type ) {
+        if( type === "rendimento" ){
+
+            id = parseInt( id );
+            var positionToRemove;
+
+            for ( var i = 0; i < data.elementos.listaRendimento.length; i++ ){
+
+                if( data.elementos.listaRendimento[i].id === id ){
+                    positionToRemove = i;
+                    break;
+                }
+            }
+
+            if( positionToRemove !== undefined ){
+                data.elementos.listaRendimento.splice( positionToRemove, 1 );
+                recalculateTotal();
+
+            } else if( positionToRemove === undefined ){
+                console.log( "Numero à excluir não encontrado" );
+            }
+            
+        } else if( type === "despesa" ){
+            id = parseInt( id )
+            var positionToRemove = -1;
+            
+            for ( var i = 0; i < data.elementos.listaDespesa.length; i++ ){
+                if ( data.elementos.listaDespesa[i].id === id ){
+                    positionToRemove = i;
+                    break;
+                }
+            }
+
+            if( positionToRemove !== -1 ){
+                data.elementos.listaDespesa.splice( positionToRemove, 1 );
+                recalculateTotal();
+
+            } else if( positionToRemove === -1 ){
+                console.log( "Numero à excluir não encontrado" );
+            }
+
+        } else{
+            console.log( "Tipo não identificado, remoção falhou!" );
+        }
+
+    }
+
+    var recalculateTotal = function(){
+       
+        var rendimentoTotal = 0;
+        var despesaTotal = 0;
+
+        // // Calcula total de despesa usando Lista de Despesas.
+        // for( var i = 0; i < data.elementos.listaDespesa.length; i++ ){
+        //     despesaTotal += parseFloat( data.elementos.listaDespesa[ i ].value );
+        // }
+        data.elementos.listaDespesa.forEach( function( element ) {
+            despesaTotal = despesaTotal + parseFloat( element.value ); 
+        } )
+        
+        //Atualiza o Objeto com o total de despesa
+        data.elementos.totalDespesa = despesaTotal;
+
+        // Calcula total de rendimento usando Lista de Rendimentos.
+        // for( var i = 0; i < data.elementos.listaRendimento.length; i++ ){
+        //     rendimentoTotal += parseFloat( data.elementos.listaRendimento[ i ].value );
+        // }
+        data.elementos.listaRendimento.forEach( function( element ){
+            rendimentoTotal = rendimentoTotal + parseFloat( element.value );
+        } );
+
+        // Atualiza o Objeto com o total de rendimento
+        data.elementos.totalRendimento = rendimentoTotal;
+        
+
+        // Atualizando economia total = rendimento total - despesa total;
+        data.elementos.economiaTotal = rendimentoTotal - despesaTotal;
+        
+        // Calcula porcentagem de cada elemento da lista de despesa.
+        // for( var i = 0; i < data.elementos.listaDespesa.length; i++ ){
+        //     data.elementos.listaDespesa[ i ].calculatePercentage( data.elementos.totalRendimento );
+        // }
+        
+        data.elementos.listaDespesa.forEach( function( element ){
+            element.calculatePercentage( data.elementos.totalRendimento );
+        } )
+
+        // Atualiza porcentagem de despesa total
+        if( rendimentoTotal === 0 ){
+            data.elementos.totalPorcentagemDespesa = -1;
+        } else {
+            data.elementos.totalPorcentagemDespesa = ( parseFloat( data.elementos.totalDespesa ) / rendimentoTotal ).toFixed( 2 ) * 100;
+        }
+            
+        
+    }
+
     return {
         getData: function(){
             return data;
@@ -297,6 +443,10 @@ var DataController = ( function() {
 
         },
 
+        removeData: function( id, type ){
+            removeDataFromList( id, type );
+        },
+
         validateData: function( arrayInputs ){
             
             // Descrição vazia
@@ -312,53 +462,8 @@ var DataController = ( function() {
             }
         },
 
-        // Melhorar isso aqui!
-        calculateTotal: function(){
-            var rendimentoTotal = 0;
-            var despesaTotal = 0;
-
-            // // Calcula total de despesa usando Lista de Despesas.
-            // for( var i = 0; i < data.elementos.listaDespesa.length; i++ ){
-            //     despesaTotal += parseFloat( data.elementos.listaDespesa[ i ].value );
-            // }
-            data.elementos.listaDespesa.forEach( function( element ) {
-                despesaTotal = despesaTotal + parseFloat( element.value ); 
-            } )
-           
-            //Atualiza o Objeto com o total de despesa
-            data.elementos.totalDespesa = despesaTotal;
-
-            // Calcula total de rendimento usando Lista de Rendimentos.
-            // for( var i = 0; i < data.elementos.listaRendimento.length; i++ ){
-            //     rendimentoTotal += parseFloat( data.elementos.listaRendimento[ i ].value );
-            // }
-            data.elementos.listaRendimento.forEach( function( element ){
-                rendimentoTotal = rendimentoTotal + parseFloat( element.value );
-            } );
-
-            // Atualiza o Objeto com o total de rendimento
-            data.elementos.totalRendimento = rendimentoTotal;
-            
-
-            // Atualizando economia total = rendimento total - despesa total;
-            data.elementos.economiaTotal = rendimentoTotal - despesaTotal;
-            
-            // Calcula porcentagem de cada elemento da lista de despesa.
-            // for( var i = 0; i < data.elementos.listaDespesa.length; i++ ){
-            //     data.elementos.listaDespesa[ i ].calculatePercentage( data.elementos.totalRendimento );
-            // }
-            
-            data.elementos.listaDespesa.forEach( function( element ){
-                element.calculatePercentage( data.elementos.totalRendimento );
-            } )
-
-            // Atualiza porcentagem de despesa total
-            if( rendimentoTotal === 0 ){
-                data.elementos.totalPorcentagemDespesa = -1;
-            } else {
-                data.elementos.totalPorcentagemDespesa = ( parseFloat( data.elementos.totalDespesa ) / rendimentoTotal ).toFixed( 2 ) * 100;
-            }
-            
+        calculateTotal() {
+            recalculateTotal();
         }
     }
 
@@ -370,33 +475,96 @@ var MainController = ( function( dtCtrl, uiCtrl ) {
 
     var inputs = uiCtrl.getDOMElements();
 
+    //função addData - MainController
+    var addData = function( ){
+        var inputs = uiCtrl.inputListener();
+            
+        if( dtCtrl.validateData( inputs ) === true ){
+            var totalData, data;
+            dtCtrl.addData( inputs[0], inputs[1], inputs[2] );
+            console.log( "Adicionado com sucesso!" );
+            
+
+            dtCtrl.calculateTotal();
+            
+            totalData = dtCtrl.getTotal();
+            data = dtCtrl.getData();
+
+            uiCtrl.addUiData( data.elementos, totalData, inputs[0] );
+
+            uiCtrl.refreshDisplayData( data.elementos );
+
+        }else {
+            console.log( "Elemento novo não adicionado! Campos obrigatórios não válidos" );
+        }
+    }
+
+
     // Função setAddEventHandler
     var setAddEventHandler = function() {
         
-        inputs.btn.addEventListener( "click", function(){
-                       
-            var inputs = uiCtrl.inputListener();
-            
-            if( dtCtrl.validateData( inputs ) === true ){
-                var totalData, data;
-                dtCtrl.addData( inputs[0], inputs[1], inputs[2] );
-                console.log( "Adicionado com sucesso!" );
-                
-
-                dtCtrl.calculateTotal();
-                
-                totalData = dtCtrl.getTotal();
-                data = dtCtrl.getData();
-
-                uiCtrl.addUiData( data.elementos, totalData, inputs[0] );
-
-                uiCtrl.refreshDisplayData( data.elementos );
-
-            }else {
-                console.log( "Elemento novo não adicionado! Campos obrigatórios não válidos" );
+        inputs.btn.addEventListener( "click", addData );
+        
+        inputs.form.addEventListener( "keypress", function( e ) {
+            if( e.keyCode === 13 ){
+                addData();
             }
+        })
+
+        
+    }
+    
+    var setRemoveEventHandler = function() {
+        
+        console.log( "Manipulador de evento de remoção settado")
+        
+        // rendimentoList
+        inputs.rendimentoList.addEventListener( "click", function( e ){
             
-        });
+            // caso clique no icone de exclusão pegar o id do elemento clicado
+            if( e.target.className === "ion-ios-close-outline" ){
+                // console.log( e.target.parentNode.parentNode.parentNode.parentNode.id );
+                
+                var tempID = e.target.parentElement.parentElement.parentElement.parentElement.id;
+                tempID = tempID.split("-");
+                tempID = tempID[1];
+                // console.log( tempID );
+
+                // remover elemento de Data
+                dtCtrl.removeData( tempID, "rendimento" );
+
+                data = dtCtrl.getData()
+
+                // remover elemento da UI
+                uiCtrl.removeData( tempID, "rendimento" );
+                uiCtrl.refreshDisplayData( data.elementos );
+                
+
+            }
+
+        } );
+
+        // despesaList
+        inputs.despesaList.addEventListener( "click", function( e ) {
+            
+            // Remover elemento que teve evento clicado no icone "x"
+            if( e.target.className === "ion-ios-close-outline" ){
+                var tempID = e.target.parentElement.parentElement.parentElement.parentElement.id;
+                tempID = tempID.split("-");
+                tempID = tempID[1];
+                
+                // removendo elemento da lista de despesas.
+                dtCtrl.removeData( tempID, "despesa" );
+
+                data  = dtCtrl.getData();
+
+                // remover elemento da UI
+                uiCtrl.removeData( tempID, "despesa" );
+                uiCtrl.refreshDisplayData( data.elementos );
+            }
+
+
+        } );
 
     }
 
@@ -405,6 +573,7 @@ var MainController = ( function( dtCtrl, uiCtrl ) {
         init: function(){
             console.log( "Programa inicializado" );
             setAddEventHandler();
+            setRemoveEventHandler();
             
         },
 
