62a1eff4e1dac6d351a90e1d403d4518f3a403da
luizfcneto <luizfcneto123@gmail.com>
2020-06-08 20:01:55 -0300

    *Relembrando variaveis let, const, iffes & string template literals em ES6

--- JavaScript/syntax-es6/let-const/aula01.js
+++ JavaScript/syntax-es6/let-const/aula01.js
@@ -2,76 +2,81 @@
     Aula01: Diferença entre var, const e let
 */
 
-//Testando variaveis do tipo var
-// erro
+// console.log( testeVarES5 );
+// var testeVarES5;
+// console.log( "testeVarES5 ",  testeVarES5 );
+
+// console.log( testeLetES6 );
+// let testeLetES6;
+// console.log( "testeLetES6 ", testeLetES6 );
+
+// const testeConstES6;
+// // Erro de sintaxe, faltando atribuição ao declarar uma variavel constante.
+// console.log( "testeConstES6 ", testeConstES6 );
+
+
+// Testando variaveis do tipo var
+
+// erro variável não declarada.
 // console.log( testeVar );
+
+
 function funcaoTesteVar(){
-    var testeVar;
+    if( true ){
+        // Não dá erro, se a variável não foi declarada 
+        // console.log( testeVar2 );
+        var testeVar2 = "variavel de escopo de função, var";
 
-    //undefined
-    console.log( testeVar );
-
-    testeVar = 5;
-    console.log( testeVar );
-
-    for( var i = 0; i < 6; i++ ){
-        testeVar++;
-        // testeVar += i;
-        console.log( testeVar );
     }
-
-    testeVar = 6;
-
-    if( testeVar > 10 ){
-        testeVar = 3;
-        console.log( testeVar );
     
-    }else{
-        console.log( testeVar );
+    // Funciona porque testeVar2 é uma variavel de escopo de função, logo ela está acessível por toda a função funcaoTesteVar2
+    console.log( testeVar2 );
+
+    var i = 25;
+    console.log( i );
+
+    for( var i = 0; i < 10; i++ ){
+        console.log( i );
     }
+
+    // A variavel i se sobrepõe, logo o resultado esperado abaixo é 10. e não 25, pois ambas representam a mesma variável, porquê estão no mesmo escopo de função.
+    console.log( i );
 
 }
 // funcaoTesteVar();
-//erro
-// console.log( testeVar );     
+
 
 
 //Testando variaveis do tipo let
-//erro, testeLet não definido
-// console.log( testeLet );
 
 function funcaoTesteLet(){
-    let testeLet;
+    if( true ){
 
-    //undefined
-    console.log( testeLet );
+        // erro, não podemos manipular a variavel tipo let, antes de declará-la e atribui-la algum valor.
+        // console.log( testeLet2 );
+        let testeLet2 = "Variavel de escopo de bloco de instrução";
+        
+        //  Bloco de instrução if acessível para a variavel testeLet2
+        // console.log( testeLet2 );
 
-    testeLet = 7;
-    console.log( testeLet );
-
-    for( let i = 0; i < 3; i++ ){
-        testeLet++;
-        // testeLet += i;
-        console.log( testeLet );
     }
 
-    //erro: i não está contido nesse escopo de bloco.
-    // console.log( i );
+    // Erro, bloco de instrução inacessível para testeLet2, variável não definida nesse bloco de instrução
+    // console.log( testeLet2 );
 
-    if( testeLet > 10 ){
-        let testeLet2 = 1;
-        testeLet = 3;
-        console.log( testeLet + " teste2: " + testeLet2 );
-    
-    }else{
-        //erro: teste2, não estpa contido nesse escopo de bloco.
-        // console.log( testeLet + " teste2: " + testeLet2 );
+    // escopo desse bloco de instrução esse i = 20
+    let i = 20;
+
+    for( let i = 0; i < 5; i++ ){
+        // i = 0, 1, 2, 3, 4; escopo desse bloco de instrução esse i varia de 0 até 4
+        console.log( i );
     }
 
+    // volta pro bloco de instrução anterior que i = 20, cada escopo de instrução i representa uma variavel diferente, mantendo assim o seu valor de acordo com seu escopo, mesmo que o nome da variavel seja a mesma.
+    console.log( i );
 }
+
 // funcaoTesteLet();
-//erro, testeLet não definido
-// console.log( testeLet );
 
 //Testando variaveis do tipo const:
 //erro, não podemos acessar testConst antes de sua inicialização
@@ -80,23 +85,19 @@ function funcaoTesteLet(){
 const testeConst = 13;
 
 function funcaoTesteConst(){
-    console.log( testeConst );
 
-    const testeConst2 = 5;
+    // console.log( testeConst2 );
+    const testeConst2 = "Variavel const2";
 
-    //Erro, não podemos alterar o valor de uma constante já inicializada
-    // testeConst = 10;
-    // testeConst2 = testeConst + testeConst2;
-
-    if( testeConst === testeConst2 ){
-        console.log( "Entrou no if" );
-    
-    }else{
-        console.log( "teste1: " +  testeConst + " teste2: " + testeConst2 );  
+    if( true ){
+        const testeConst3 = "Variavel const3";
     }
+    
+    // erro, variavel testeConst3 inacessível nesse bloco de instrução. 
+    // console.log( "testeConst3 ", testeConst3 );
 }
-//erro, testeConst2 não está definido 
-// console.log( testeConst2 );
-// console.log( testeConst );
 
-// funcaoTesteConst();
+// Erro, variável testeConst2 inacessível neste bloco de instrução.
+// console.log( "testeConst2 ", testeConst2 );
+
+funcaoTesteConst();
