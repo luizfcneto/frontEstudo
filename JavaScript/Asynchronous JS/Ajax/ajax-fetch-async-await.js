41842f6fa1c9c6b21179ac7dc095cfe12fc65b8c
luizfcneto <luizfcneto123@gmail.com>
2020-07-02 21:42:27 -0300

    *Praticando Consumo API climatempo & accuweather

--- JavaScript/Asynchronous JS/Ajax/ajax-fetch-async-await.js
+++ JavaScript/Asynchronous JS/Ajax/ajax-fetch-async-await.js
@@ -5,6 +5,7 @@
 */
 
 
+// Busca realizada utilizando API metaweather
 async function buscaClimaAW( woeid ){
     
     // faz tratamento de um erro de requisição.
@@ -35,7 +36,7 @@ async function buscaClimaAW( woeid ){
 
 }
 
-let data;
+// let data;
 
 // id RJ:
 buscaClimaAW( 455825 ).then( ( response ) => {
@@ -60,3 +61,61 @@ buscaClimaAW( 0 ).then( response => {
     data = response;
     console.log( data );
 } )
+
+// token da API climatempo advisor: 
+// const chaveToken = "Insira sua chave aqui";
+
+
+// // Busca Realizada utilizando a API climatempo advisor:
+
+// async function getBrazilianCitysList( chaveToken ){
+//     const response = await fetch( `http://apiadvisor.climatempo.com.br/api/v1/locale/city?country=BR&token=${ chaveToken }` );
+
+//     const data = await response.json();
+//     console.log( data );
+// }
+
+
+// async function buscaCidadePorId( id, chaveToken ){
+//     const response = await fetch( `http://apiadvisor.climatempo.com.br/api/v1/locale/city/${ id }?token=${ chaveToken }` );
+
+//     const data = await response.json();
+//     console.log( data );
+// }
+
+// // lista de cidades no Brasil:
+// getBrazilianCitysList( chaveToken );
+
+// // id da Cidade do Estado do Rio de Janeiro = 5959
+// buscaCidadePorId( 5959, chaveToken );
+
+// token da API accuweather:
+// const chaveToken = "Insira sua chave aqui"
+
+// async function getTemperatureCityById( id, keyToken ){
+//     const response = await fetch( `https://cors-anywhere.herokuapp.com/http://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${ keyToken }` );
+
+//     const data = await response.json();
+//     console.log( `Temperatura: ${ data[ 0 ].Temperature.Metric.Value } ${ data[ 0 ].Temperature.Metric.Unit } `  );
+
+// }
+
+// async function findCityByString( string, keyToken ){
+//     const response = await fetch( `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${ keyToken }&q=${ string }` );
+
+//     const data = await response.json();
+//     console.log( `Estado: ${ data[ 0 ].LocalizedName } \nID: ${ data[ 0 ].Key } \nLocalizada no País: ${ data[ 0 ].Country.LocalizedName } ` );
+// }
+
+// // procura id da cidade 
+// findCityByString( "Minas Gerais", chaveToken );
+
+// // id: 45449 é o ID do Estado do Rio de Janeiro
+// // getTemperatureCityById( 45449, chaveToken );
+
+
+// // id: 45881 é o ID do Estado de São Paulo
+// // getTemperatureCityById( 45881, chaveToken );
+
+// // id: 2728201 é o ID do Estado de Minas Gerais
+// getTemperatureCityById( 2728201, chaveToken ); 
